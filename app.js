d968e68bc27ceccf058e38135cd50548b42100f8
Newton <kamaunewton78@gmail.com>
2020-07-09 23:19:34 +0300

    use ejs

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set("view engine", "ejs");
+
 
 app.get("/", function (req, res) {
-    res.send("Hello");
+   res.send("Hello")
 });
 
 app.listen(3000, function () {
