9ce1c6020650ac12402d7888b18f2d01ec3cc5e7
Newton <kamaunewton78@gmail.com>
2020-07-10 00:04:09 +0300

    added days of the week

--- app.js
+++ app.js
@@ -6,20 +6,46 @@ const bodyParser = require("body-parser");
 const app = express();
 app.set("view engine", "ejs");
 
-
 app.get("/", function (req, res) {
-    var today = new Date();
-    var currentDay = new Date().getDate();
-    var day = "";
-    if (currentDay == 9 || currentDay == 5) {
-        day = "The weekend is here , parte after parte";
-          res.render("list", { kindOfDay: day });
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
 
-    } else {
-        res.send("You have to work harder and longer man");
-    }
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Wednesday";
+      break;
+    case 3:
+      day = "Thursday";
+      break;
+    case 4:
+      day = "Friday";
+      break;
+    case 5:
+      day = "Saturday";
+      break;
+    case 6:
+      day = "Sunday";
+      break;
+    default:
+      console.log("Error current day = " + currentDay);
+  }
+
+  // if (currentDay == 9 || currentDay == 5) {
+  //     day = "The weekend is here , parte after parte";
+
+  // } else {
+  //     day = "weekday";
+  // }
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
-    console.log("Server started on port 3000.");
+  console.log("Server started on port 3000.");
 });
