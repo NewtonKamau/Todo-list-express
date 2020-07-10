8671591885a7735bdb00b357393c8fe607e1eb1f
Newton <kamaunewton78@gmail.com>
2020-07-10 12:37:24 +0300

    Used toLocaleDate() to get the exact day and date of the week

--- app.js
+++ app.js
@@ -8,35 +8,13 @@ app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
-
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Wednesday";
-      break;
-    case 3:
-      day = "Thursday";
-      break;
-    case 4:
-      day = "Friday";
-      break;
-    case 5:
-      day = "Saturday";
-      break;
-    case 6:
-      day = "Sunday";
-      break;
-    default:
-      console.log("Error current day = " + currentDay);
-  }
-
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  var day = today.toLocaleDateString("en-US", options);
+  console.log(day);
   res.render("list", { kindOfDay: day });
 });
 
